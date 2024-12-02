e7f4cbba4ade42b39dd8f58db82ae6e47ec4eca7
rebecaaras <rebecaaraujo2013@gmail.com>
2024-12-02 20:28:30 -0300

    new: Add string exercises

---
+++ strings.js
@@ -0,0 +1,10 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr)
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+console.log(lastNameLength);
