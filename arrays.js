ac864915b80c7dc2b4a35b8c40cd9470088ae3fd
rebecaaras <rebecaaraujo2013@gmail.com>
2024-12-02 21:01:08 -0300

    new: Add array exercises

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const myList = [];
+myList.push(["Chocolate Bar", 15])
+myList.push(["Eggs", 11])
+myList.unshift(["Coffee", 20])
+myList.unshift(["Bread", 8.5])
+myList.shift()
+myList.push(["Nuts", 8.5])
+myList.push(["Soap", 5])
+console.log(myList)
